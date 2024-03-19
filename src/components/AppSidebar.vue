677d74b0b24ae2a17905e920e28eb0967e0bf483
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2024-03-20 02:55:32 +0300

    [refactored] sidebar tabs

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -7,27 +7,13 @@
             </BrandIdentity>
             <TabsGroup>
                 <RouterLink
-                    to="/"
+                    v-for="tab in tabs"
+                    :key="tab.route"
+                    :to="tab.route"
                     :class="routerLinkClasses"
                     :active-class="routerLinkActiveClasses">
-                    <TabItem title="الرئيسية">
-                        <ChartDonut />
-                    </TabItem>
-                </RouterLink>
-                <RouterLink
-                    to="/contacts"
-                    :class="routerLinkClasses"
-                    :active-class="routerLinkActiveClasses">
-                    <TabItem title="جهات الاتصال">
-                        <TwoUsers />
-                    </TabItem>
-                </RouterLink>
-                <RouterLink
-                    to="/deals"
-                    :class="routerLinkClasses"
-                    :active-class="routerLinkActiveClasses">
-                    <TabItem title="العروض">
-                        <DealTag />
+                    <TabItem :title="tab.title">
+                        <component :is="tab.icon"></component>
                     </TabItem>
                 </RouterLink>
             </TabsGroup>
@@ -52,6 +38,24 @@ import ChartDonut from './icons/ChartDonut.vue';
 import TwoUsers from './icons/TwoUsers.vue';
 import DealTag from './icons/DealTag.vue';
 
+// Data
+const tabs = [
+    {
+        title: 'الرئيسية',
+        route: '/',
+        icon: ChartDonut
+    },
+    {
+        title: 'جهات الاتصال',
+        route: '/contacts',
+        icon: TwoUsers
+    },
+    {
+        title: 'العروض',
+        route: '/deals',
+        icon: DealTag
+    }
+];
 const routerLinkClasses = 'hover:bg-slate-800 rounded-lg text-slate-600';
 // [x] fix `hover:bg-primary-500` not working
 const routerLinkActiveClasses =
--- src/components/BrandIdentity.vue
+++ src/components/BrandIdentity.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="text-2xl font-bold">
+    <div class="flex gap-2 text-2xl font-bold">
         <slot name="logo"></slot>
         <slot name="name"></slot>
     </div>
