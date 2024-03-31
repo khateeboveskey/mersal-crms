e40164e768ba1fd962d40c8c37cddccb184df554
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2024-03-31 18:09:44 +0300

    [fixed] mobile tabs position

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,11 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    plugins: [vue(), VueDevTools()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    base: '/vue-gh-actions-test/'
+});
