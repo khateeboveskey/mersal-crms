2509c26c1b99bbee2ef87d9f3794777a3fe2840b
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2024-04-08 22:40:27 +0300

    Add locations CRUD

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="flex flex-col flex-nowrap overflow-x-hidden md:flex-row">
         <AppSidebar />
-        <span clas=""></span>
         <RouterView
-            class="no-scrollbar after:content[''] w-full overflow-y-scroll after:absolute after:right-0 after:top-0 after:-z-10 after:h-[-webkit-fill-available] after:w-[inherit] after:bg-inherit" />
+            class="no-scrollbar after:content[''] h-screen w-full overflow-y-scroll p-10 after:absolute after:right-0 after:top-0 after:-z-10 after:h-[-webkit-fill-available] after:w-[inherit] after:bg-inherit" />
     </div>
 </template>
 <script setup>
