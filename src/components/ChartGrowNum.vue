ac73c8384f028735b041ac5e3b68bdc8ea5ee27b
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2024-04-27 15:05:51 +0300

    Styling updates
    - Add loader
    - ChartGrowNum made mode-native

--- src/components/ChartGrowNum.vue
+++ src/components/ChartGrowNum.vue
@@ -2,8 +2,8 @@
     <div class="w-full max-w-sm rounded-lg bg-white p-4 text-white shadow md:p-6 dark:bg-gray-800">
         <h5 class="mb-3 text-xl text-gray-500">{{ props.chartTitle }}</h5>
         <div class="flex flex-row gap-2">
-            <component class="h-10" :is="props.icon"></component>
-            <p class="text-4xl font-bold">{{ props.chartData }}</p>
+            <component class="h-10 text-slate-600 dark:text-white" :is="props.icon"></component>
+            <p class="text-4xl font-bold text-slate-600 dark:text-white">{{ props.chartData }}</p>
         </div>
     </div>
 </template>
